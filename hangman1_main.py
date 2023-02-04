75961da8fc784717059ecae5c1cc0288591f554f
Joshua Bennett <jb.skimjazz@outlook.com>
2023-02-04 15:58:39 +1000

    Added steps.txt file, cut&pasted todo list to steps.txt, steps.txt added to gitignore

--- hangman1_main.py
+++ hangman1_main.py
@@ -1,7 +1,9 @@
-# :Todo: Add a function to check if the user has already guessed a letter
+# : Coder Academy - T1A3 Terminal Application
+# : Joshua Bennett
+# : ID 13399
+# : February 2023
+# : Hangman Game
+# : This project is assignment-3 of term-1 for Coder Academy standard delivery. Ref to the README.md file for more
+# : information.
 
-
-
-
-# :Todo: Add a function to check if the user has already guessed the word
 
