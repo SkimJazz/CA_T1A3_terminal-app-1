d7ebe42be4eb4ffee8d52a4f4715cb821e067cf0
Joshua Bennett <jb.skimjazz@outlook.com>
2023-02-05 12:05:19 +1000

    Added error check random genWord with matching letter spacing

--- hangman1_main.py
+++ hangman1_main.py
@@ -21,7 +21,7 @@ wordList = [
 ]
 
 # : Todo-2: Add a random word generator
-randomWord = random.choice(wordList)
+genWord = random.choice(wordList)
 
 # : Todo-3: Ask the user to guess a letter
     # : todo-3.1: need user input with a prompt
@@ -30,20 +30,30 @@ randomWord = random.choice(wordList)
     # : todo-3.4: check if the user input is a letter
         # : todo-3.4.1: if not a letter, ask the user to enter a letter
 
+# : Test the random word generator and user input
+print(f"{genWord}")
+displayWord = []
+for _ in range(len(genWord)):
+    displayWord += "_"
+print(displayWord)
+
+
 # Error handling for user input-> only letters allowed
 while True:
-
-    # User input and validation
+    # User input and validation accounting for case sensitivity and Linux/iSO compatibility
     uGuess = input("Guess a letter: ").lower()
-
-    # 'isalpha' checks if the input is a letter -> iSO & Linux compatible method
+    # 'isalpha' checks if the input is a letter -> Linux/iSO compatible method
     if uGuess.isalpha() and len(uGuess) == 1:
-
-        # break out of the loop if the input is valid
+        # break out of the loop if input is valid
         break
     print("Invalid input. Please enter a single letter.")
 
+
 # uGuess = input("Guess a letter: ").lower()
+
+
+
+
 
 # : Todo-4: Check if the letter is in the word
     # : todo-4.1: loop through the randomWord using a for loop
@@ -51,7 +61,7 @@ while True:
     # : todo-4.2.1: if the letter is in the randomWord, print a message
     # : todo-4.2.2: else letter is not in the randomWord, print a message
 
-for letter in randomWord:
+for letter in genWord:
     if letter == uGuess:
         print("Correct! You guessed a letter")
     else:
