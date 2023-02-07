9df3ce651c207dfa4d5de84e0f5bc86c5a064561
Joshua Bennett <jb.skimjazz@outlook.com>
2023-02-07 17:29:09 +1000

    "Game functioning as expected, code clean-up"

--- hangman1_main.py
+++ hangman1_main.py
@@ -18,66 +18,67 @@ genWord = random.choice(hangman1_wordList.wordList)
 wordLength = len(genWord)
 
 
-# User lives
+# : User lives
 lives = 6
 
 
-# Display intro -> Dyslexic Hangman
+# : Display intro -> Dyslexic Hangman
 print(intro)
 
 
-# Test the random word generator and user input
+# : Test the random word generator and user input
 print(f"Word test is: {genWord}")
 
-# Create a list of underscores(blanks) to display to user
+# : Create a list of underscores(blanks) to display to user
 displayWord = []
 for _ in range(wordLength):
     displayWord += "_"
 
 endGame = False
 
-# Game play loop
+# : Game play loop
 while not endGame:
 
-    # User input for letter guess and exiting game
-    uGuess = input("Guess a letter or type 'Shift + # then enter' to quit game: ").lower()
+    # : User input for letter guess and exiting game
+    uGuess = input("Guess a letter or press 'Zero' and 'Enter' to quit game: ").lower()
 
-    # User option to exit game at letter guessing stage
-    # Cant use a letter as a break-out condition as letter could be in the word list
-    if uGuess == "#":
+    if uGuess in displayWord:
+        print("You already guessed that letter")
+
+    # : User option to exit game at letter guessing stage
+    # : Cant use a letter as a break-out condition as letter could be in the word list
+    if uGuess == "0":
         print("Yeah Nice! Catch ya next time")
         endGame = True
         break
 
-    # User input error handling-> letters only-> 'isalpha()' Linux/iSO compatible method
+    # : User input error handling-> letters only-> 'isalpha()' Linux/iSO compatible method
     if uGuess.isalpha() and len(uGuess) == 1:
 
-        # Cross-reference user letter guess with letter in genWord
+        # : Cross-reference user letter guess with letter in genWord
         for letterPosition in range(wordLength):
             letter = genWord[letterPosition]
 
-            # If the letter is in word, replace the underscore with the letter
+            # : If the letter is in word, replace the underscore with the letter
             if letter == uGuess:
                 displayWord[letterPosition] = letter
 
-        print(displayWord)
+        print(" ".join(displayWord))
 
-        # If the letter is not in the word, remove a life
+        # : If the letter is not in the word, remove a life
         if uGuess not in genWord:
+            print(f"You guessed {uGuess}, that's not in the word. You lose a life")
             lives -= 1
             if lives == 0:
                 endGame = True
                 print("You lose!")
 
-        # While loop BREAK OUT condition -> if no more underscores in the displayWord list, user has won game
+        # : While loop BREAK OUT condition -> if no more underscores in the displayWord list, user has won game
         elif "_" not in displayWord:
             endGame = True
             print("You win!")
 
-        # : Method-1 - using 'import hangman1_ascii' and calling the stages from hangman1_ascii.py
-        # print(hangman1_ascii.stage[lives])
-
-        # : Method-2 - using 'from hangman1_ascii import stage' and calling the stages from hangman1_ascii.py
+        # : Method - using 'from hangman1_ascii import stage' and calling the stages from hangman1_ascii.py
         print(stage[lives])
 
     else:
