f17598b0b9ecaf915405dea75bf2836932d39505
Joshua Bennett <jb.skimjazz@outlook.com>
2023-02-09 14:47:36 +1000

    Wrapped code in play_game function

--- hangman1_main.py
+++ hangman1_main.py
@@ -13,128 +13,128 @@ import hangman1_wordList
 from hangman1_ascii import intro, stage
 
 
-# : Generate a random word from the wordList
-genWord = random.choice(hangman1_wordList.wordList)
+def play_game():
 
+    # : Generate a random word from the wordList
+    gen_word = random.choice(hangman1_wordList.wordList)
 
-# ============= Dyslexic letter shuffle of random word =============
+    # ============= Dyslexic letter shuffle of random word =============
 
-# : Convert the randomly generated word to a list
-word_list = list(genWord)
+    # : Convert the randomly generated word to a list
+    word_list = list(gen_word)
 
-# : Shuffle letters in the list
-random.shuffle(word_list)
+    # : Shuffle letters in the list
+    random.shuffle(word_list)
 
-# : Convert the shuffled letters back to a string
-shuffled_word = "".join(word_list)
+    # : Convert the shuffled letters back to a string
+    shuffled_word = "".join(word_list)
 
-# : Get the length of shuffled word
-wordLength = len(shuffled_word)
+    # : Get the length of shuffled word
+    word_length = len(shuffled_word)
 
+    # : User lives
+    lives = 6
 
-# : User lives
-lives = 6
+    # : Display intro -> Dyslexic Hangman
+    print("\n Hey! Welcome to", "\n", intro, "\n", "\n",
+          "This Hangman game has a twist. The letters in the word are shuffled. Good luck!")
 
+    # : Random word generator test
+    # print(f"Word test is: {genWord}")
 
-# : Display intro -> Dyslexic Hangman
-print("\n Hey! Welcome to", "\n", intro, "\n", "\n",
-      "This Hangman game has a twist. The letters in the word are shuffled. Good luck!")
+    # : Create a list of underscores(blanks) to display to user
+    display_word = []
+    for _ in range(word_length):
+        display_word += "_"
 
+    end_game = False
 
-# : Test the random word generator and user input
-# print(f"Word test is: {genWord}")
+    # : =================== Read CSV file ====================
 
-# : Create a list of underscores(blanks) to display to user
-displayWord = []
-for _ in range(wordLength):
-    displayWord += "_"
+    with open("hangman1_riddles.csv", "r") as read_obj:
+        reader = csv.DictReader(read_obj)
+        rows = list(reader)
 
+        # : Search for the word in the CSV file
+        match_found = False
 
-endGame = False
+        # : Initialize var or PyCharm will lose its shit
+        riddle = ""
 
+        # : Iterate over each row in the csv using reader object
+        for row in rows:
 
-# : =================== Read CSV file ====================
+            # : Check if random genWord matches the word in the CSV file
+            if row['word'] == gen_word:
+                # : Set match_found to True
+                match_found = True
 
-with open("hangman1_riddles.csv", "r") as read_obj:
-    reader = csv.DictReader(read_obj)
-    rows = list(reader)
+                # : Read from CSV Test -> Get id, word, riddle from row
+                # id = row['id']
+                # word = row['word']
+                riddle = row['riddle']
+                break
 
-    # : Search for the word in the CSV file
-    match_found = False
+        # : Print riddle if match found
+        if match_found:
+            # print("Id:", id)
+            # print("Word:", word)
+            print("\n What am I?", riddle)
+            # print("\n")
+        # : Error message if match not found -> should only happen if the word is not in the CSV file
+        else:
+            print("Match not found.")
 
-    # : Initialize var or PyCharm will lose its shit
-    riddle = ""
+    # : =================== Game play loop ====================
 
-    # : Iterate over each row in the csv using reader object
-    for row in rows:
+    while not end_game:
 
-        # : Check if random genWord matches the word in the CSV file
-        if row['word'] == genWord:
+        # : User input for letter guess and exiting game
+        u_guess = input("\n Guess a letter or press 'Zero' and 'Enter' to quit game: ").lower()
 
-            # : Set match_found to True
-            match_found = True
+        if u_guess in display_word:
+            print("You already guessed that letter")
 
-            # : Get the id, word and riddle from the row
-            id = row['id']
-            word = row['word']
-            riddle = row['riddle']
+        # : User option to exit game at letter guessing stage
+        # : Cant use a letter as a break-out condition as letter could be in the word list
+        if u_guess == "0":
+            print("Yeah Nice! Catch ya next time")
+            end_game = True
             break
 
-    # : Print riddle if match found
-    if match_found:
-        print("Id:", id)
-        print("Word:", word)
-        print("\nWhat am I?", riddle)
-    else:
-        print("Match not found.")
+        # : User input error handling-> letters only-> 'isalpha()' Linux/iSO compatible method
+        if u_guess.isalpha() and len(u_guess) == 1:
+
+            # : Cross-reference user letter guess with letter in genWord
+            for letter_position in range(word_length):
+                # letter = genWord[letterPosition]
+                letter = shuffled_word[letter_position]
+
+                # : If the letter is in word, replace the underscore with the letter
+                if letter == u_guess:
+                    display_word[letter_position] = letter
+
+            print(" ".join(display_word))
+
+            # : If the letter is not in the word, remove a life
+            if u_guess not in gen_word:
+                print(f"You guessed {u_guess}, that's not in the word. You lose a life")
+                lives -= 1
+                if lives == 0:
+                    end_game = True
+                    print("You lose!")
+
+            # : While loop BREAK OUT condition -> if no more underscores in the displayWord list, user has won game
+            elif "_" not in display_word:
+                end_game = True
+                print(f"You win! The word was {gen_word}")
+
+            # : Method - using 'from hangman1_ascii import stage' and calling the stages from hangman1_ascii.py
+            print(stage[lives])
+
+        else:
+            print("Invalid input. Please enter a single letter.")
 
 
-# : =================== Game play loop ====================
-
-while not endGame:
-
-    # : User input for letter guess and exiting game
-    uGuess = input("Guess a letter or press 'Zero' and 'Enter' to quit game: ").lower()
-
-    if uGuess in displayWord:
-        print("You already guessed that letter")
-
-    # : User option to exit game at letter guessing stage
-    # : Cant use a letter as a break-out condition as letter could be in the word list
-    if uGuess == "0":
-        print("Yeah Nice! Catch ya next time")
-        endGame = True
-        break
-
-    # : User input error handling-> letters only-> 'isalpha()' Linux/iSO compatible method
-    if uGuess.isalpha() and len(uGuess) == 1:
-
-        # : Cross-reference user letter guess with letter in genWord
-        for letterPosition in range(wordLength):
-            # letter = genWord[letterPosition]
-            letter = shuffled_word[letterPosition]
-
-            # : If the letter is in word, replace the underscore with the letter
-            if letter == uGuess:
-                displayWord[letterPosition] = letter
-
-        print(" ".join(displayWord))
-
-        # : If the letter is not in the word, remove a life
-        if uGuess not in genWord:
-            print(f"You guessed {uGuess}, that's not in the word. You lose a life")
-            lives -= 1
-            if lives == 0:
-                endGame = True
-                print("You lose!")
-
-        # : While loop BREAK OUT condition -> if no more underscores in the displayWord list, user has won game
-        elif "_" not in displayWord:
-            endGame = True
-            print(f"You win! The Animal was {genWord}")
-
-        # : Method - using 'from hangman1_ascii import stage' and calling the stages from hangman1_ascii.py
-        print(stage[lives])
-
-    else:
-        print("Invalid input. Please enter a single letter.")
+if __name__ == "__main__":
+    play_game()
