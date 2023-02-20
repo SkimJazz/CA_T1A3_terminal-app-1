1963568b2663e4a754a388419e438d6810fe4736
Joshua Bennett <jb.skimjazz@outlook.com>
2023-02-20 11:06:54 +1000

    Added Bash Script to run game

---
+++ run_game.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# shellcheck disable=SC2034
+for file in *.py; do
+#  echo "Reading $file..."
+  sleep 0.05
+done
+
+
+# Run Terminal App
+sleep 1.2
+python3 hangman1_main.py
+
+
+
